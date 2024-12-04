1250dda7b8ce692aa54aada4caddb4d49866a4b6
Jessica Cheng <jessicacheng.code@gmail.com>
2024-12-04 16:46:59 -0500

    wip

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,15 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        border: '#e5e7eb', // Add your desired border color
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
