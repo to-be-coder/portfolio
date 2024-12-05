0e85f4f805356cc68cd91568dbacfd0e21f55eb6
Jessica Cheng <jessicacheng.code@gmail.com>
2024-12-05 18:24:04 -0500

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,12 @@ export default function Home() {
   return (
     <>
       <Hero />
+      <div className="container mx-auto my-24 pb-48 space-y-16">
+        <h2 className="text-5xl font-semibold text-center text-gray-900 leading-relaxed">
+          &ldquo;<span className="bg-[#00a6ff]/20">Simple</span> design, lasting impact. I design with purpose, creating clear and lasting <span className="bg-[#00a6ff]/20">connections</span> between
+          products and users.&rdquo;
+        </h2>
+      </div>
       <div className="container mx-auto my-8 space-y-8">
         <Card className="w-full shadow-none">
           <div className="flex">
