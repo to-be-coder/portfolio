e6d100829d1ce27d0b3c442faa02907ab9a94171
Jessica Cheng <jessicacheng.code@gmail.com>
2024-12-04 19:59:03 -0500

    wip

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,7 +4,7 @@ import { Button } from './button'
 export default function Header() {
   return (
     <header>
-      <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="text-lg font-semibold text-gray-900">
             Jessica Cheng
@@ -19,16 +19,16 @@ export default function Header() {
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+          <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
             Home
           </a>
-          <a href="craft" className="text-sm/6 font-semibold text-gray-900">
+          <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
             Craft
           </a>
-          <a href="/about" className="text-sm/6 font-semibold text-gray-900">
+          <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
             About
           </a>
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+          <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
             CV
           </a>
         </div>
