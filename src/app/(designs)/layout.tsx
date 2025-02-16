81ea0fe2efd32a33061438ef843624e1509a6dab
Jessica Cheng <jessicacheng.code@gmail.com>
2025-02-16 10:45:43 -0500

    wip

--- src/app/(designs)/layout.tsx
+++ src/app/(designs)/layout.tsx
@@ -1,5 +1,8 @@
 'use client'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
 import Navigation from '@/components/ui/side-navigation'
+import { usePathname } from 'next/navigation'
+import type { JSX } from 'react'
 
 // Menu items.
 const navItems = [
@@ -15,43 +18,43 @@ const navItems = [
   },
 ]
 export default function DesignsLayout({ children }: { children: React.ReactNode }) {
-  // const pathname = usePathname()
-  // const paths = pathname.split('/').filter(Boolean)
+  const pathname = usePathname()
+  const paths = pathname.split('/').filter(Boolean)
 
-  // const generateBreadcrumbs = () => {
-  //   const breadcrumbs: JSX.Element[] = []
-  //   let href = ''
+  const generateBreadcrumbs = () => {
+    const breadcrumbs: JSX.Element[] = []
+    let href = ''
 
-  //   paths.forEach((path, index) => {
-  //     href += `/${path}`
-  //     const formattedPath = path
-  //       .split('-')
-  //       .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-  //       .join(' ')
+    paths.forEach((path, index) => {
+      href += `/${path}`
+      const formattedPath = path
+        .split('-')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ')
 
-  //     if (index < paths.length - 1) {
-  //       breadcrumbs.push(
-  //         <BreadcrumbItem key={href}>
-  //           <BreadcrumbLink className="text-gray-500" href={href}>
-  //             {formattedPath}
-  //           </BreadcrumbLink>
-  //           <BreadcrumbSeparator />
-  //         </BreadcrumbItem>
-  //       )
-  //     } else {
-  //       breadcrumbs.push(
-  //         <BreadcrumbItem key={href}>
-  //           <BreadcrumbPage className="text-black">{formattedPath}</BreadcrumbPage>
-  //         </BreadcrumbItem>
-  //       )
-  //     }
-  //   })
+      if (index < paths.length - 1) {
+        breadcrumbs.push(
+          <BreadcrumbItem key={href}>
+            <BreadcrumbLink className="text-gray-500" href={href}>
+              {formattedPath}
+            </BreadcrumbLink>
+            <BreadcrumbSeparator />
+          </BreadcrumbItem>
+        )
+      } else {
+        breadcrumbs.push(
+          <BreadcrumbItem key={href}>
+            <BreadcrumbPage className="text-black">{formattedPath}</BreadcrumbPage>
+          </BreadcrumbItem>
+        )
+      }
+    })
 
-  //   return breadcrumbs
-  // }
+    return breadcrumbs
+  }
 
   return (
-    <div className="mt-8  max-w-7xl  min-h-screen  mx-auto">
+    <div className="flex flex-colmt-8  max-w-7xl  min-h-screen  mx-auto">
       {/* Left side - Side Menu */}
       <aside className="w-64 hidden lg:block">
         <div className="w-64 bg-white text-black min-h-screen px-4">
@@ -64,11 +67,11 @@ export default function DesignsLayout({ children }: { children: React.ReactNode 
 
       {/* Right side - Main Content */}
       <main className="px-8 space-y-8 ">
-        {/* <nav aria-label="Breadcrumb">
+        <nav aria-label="Breadcrumb">
           <Breadcrumb>
             <BreadcrumbList>{generateBreadcrumbs()}</BreadcrumbList>
           </Breadcrumb>
-        </nav> */}
+        </nav>
         {children}
       </main>
     </div>
