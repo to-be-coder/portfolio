7b4de237b68f7c81eb9f8d7ce6649fad5fe10579
Jessica Cheng <jessicacheng.code@gmail.com>
2025-02-19 18:57:00 -0500

    wip

--- src/components/ui/cta.tsx
+++ src/components/ui/cta.tsx
@@ -8,7 +8,7 @@ export default function CtaSection() {
         <section className="py-8 md:py-16 text-center">
           <h2 className="text-4xl font-bold text-white mb-4">Let&apos;s make something users will love!</h2>
           <p className="text-[#ffffff] mb-8">If you&apos;re looking for a design engineer who combines empathy, design thinking, and technical expertise, I&apos;m here to help!</p>
-          <Button asChild className="bg-[#ff9c6a] text-white shadow-none border-none hover:bg-[#ff9c6a]/90 ">
+          <Button>
             <Link href="mailto:jessicacheng.code@gmail.com">Contact Me</Link>
           </Button>
         </section>
