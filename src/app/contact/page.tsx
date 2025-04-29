de7499d96cf933d5557ec2bf4d1ccc78d0002cc9
Jessica Cheng <jessicacheng.code@gmail.com>
2025-04-29 17:00:43 -0400

    wip

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -25,41 +25,39 @@ export default function DemoPage() {
 
   return (
     <main>
-      <div className="min-h-svh p-8 container mx-auto flex flex-col gap-8 justify-between">
-        <div className="flex flex-1">
-          <div className="mx-auto flex flex-col gap-8 justify-center items-center">
-            <div className="flex flex-col gap-8 justify-center items-center">
-              <h1 className="text-5xl font-normal">Let&apos;s get to know each other.</h1>
-              <div>
-                <div className="flex items-center gap-2">
-                  <Mail className="w-6 h-6" />
-                  <a href="mailto:jessica@thoughtful.llc" className="text-blue-600 hover:text-blue-800 text-xl font-medium">
-                    jessica@thoughtful.llc
-                  </a>
-                </div>
+      <div className="container max-w-7xl mx-auto px-8 h-[50vh] flex flex-col">
+        <div className="mx-auto flex flex-col gap-8 h-full justify-center items-center">
+          <div className="flex flex-col gap-8 justify-center items-center">
+            <h1 className="text-5xl font-normal">Let&apos;s get to know each other.</h1>
+            <div>
+              <div className="flex items-center gap-2">
+                <Mail className="w-6 h-6" />
+                <a href="mailto:jessica@thoughtful.llc" className="text-blue-600 hover:text-blue-800 text-xl font-medium">
+                  jessica@thoughtful.llc
+                </a>
               </div>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
-          <Marquee className="[--duration:60s]">
-            {firstRow.map((image, index) => (
-              <div key={index} className="mx-2">
-                <Image src={image.src} alt={image.alt} width={500} height={300} className="w-80 h-40 rounded-lg object-cover" />
-              </div>
-            ))}
-          </Marquee>
-          <Marquee reverse className="[--duration:60s]">
-            {secondRow.map((image, index) => (
-              <div key={index} className="mx-2">
-                <Image src={image.src} alt={image.alt} width={500} height={300} className="w-80 h-40 rounded-lg object-cover" />
-              </div>
-            ))}
-          </Marquee>
-          <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
-          <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
-        </div>
+      <div className="relative flex w-full flex-col items-center justify-center overflow-hidden mb-8">
+        <Marquee className="[--duration:60s]">
+          {firstRow.map((image, index) => (
+            <div key={index} className="mx-2">
+              <Image src={image.src} alt={image.alt} width={500} height={300} className="w-80 h-40 rounded-lg object-cover" />
+            </div>
+          ))}
+        </Marquee>
+        <Marquee reverse className="[--duration:60s]">
+          {secondRow.map((image, index) => (
+            <div key={index} className="mx-2">
+              <Image src={image.src} alt={image.alt} width={500} height={300} className="w-80 h-40 rounded-lg object-cover" />
+            </div>
+          ))}
+        </Marquee>
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
+        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
       </div>
 
       <CtaSection />
