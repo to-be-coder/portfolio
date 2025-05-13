16eb29342f36687235abc260e26302030ed66a20
Jessica Cheng <jessicacheng.code@gmail.com>
2025-05-13 15:35:04 -0400

    hot fix

--- src/components/ui/cta.tsx
+++ src/components/ui/cta.tsx
@@ -8,7 +8,7 @@ export default function CtaSection() {
           <h2 className="text-4xl font-bold text-white mb-4">Let&apos;s make something users will love!</h2>
           <p className="text-[#ffffff] mb-8">If you&apos;re looking for a product designer who can bridge the gap between design and code, I&apos;m here to help!</p>
           <Button variant="secondary" asChild>
-            <a href={`mailto:${process.env.NEXT_PUBLIC_EMAIL_URL}`}>Contact Me</a>
+            <a href={'/contact'}>Contact Me</a>
           </Button>
         </section>
       </div>
