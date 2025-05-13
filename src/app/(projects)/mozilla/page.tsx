5a7e87daabf571c418628c2f0c1c23bd84c4dc39
Jessica Cheng <jessicacheng.code@gmail.com>
2025-05-13 11:08:01 -0400

    wip

---
+++ src/app/(projects)/mozilla/page.tsx
@@ -0,0 +1,17 @@
+import CtaSection from '@/components/ui/cta'
+
+export default function MozillaPage() {
+  return (
+    <main>
+      {/* Hero Section */}
+      <div className="container mx-auto px-8 h-[100vh] flex flex-col">
+        <div className="flex-1 flex flex-col justify-center items-center gap-y-2 md:gap-y-4">
+          <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight leading-tight text-center">Mozilla</h1>
+          <p className="text-[32px] mt-4 text-center ">Working on it. Coming Soon</p>
+        </div>
+      </div>
+      {/* Contact Section */}
+      <CtaSection />
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         {/* Mozilla */}
 
         <div className="group relative overflow-hidden rounded-3xl bg-gray-100/80 backdrop-blur-sm p-8 h-[500px] transition-transform duration-300 hover:scale-[1.02] group">
-          <a href="/vision-track" className="w-full h-full">
+          <a href="/mozilla" className="w-full h-full">
             <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between lg:gap-8 h-full">
               <div className="flex items-center justify-center mt-4 order-last lg:order-first lg:mt-0 lg:w-2/3 relative h-[300px] lg:h-auto">
                 <div className="relative w-full h-full">
@@ -49,10 +49,12 @@ export default function Home() {
               </div>
               <div className="space-y-4 order-first lg:order-last lg:w-1/3 lg:self-start ">
                 <h3 className="text-2xl font-bold group-hover:text-secondary">Mozilla</h3>
-                <p className="text-lg">Competitive analysis and user interviews for B2B SaaS startup</p>
+                <p className="text-lg">Coming Soon</p>
                 <div className="flex flex-wrap gap-2">
-                  <span className="inline-block rounded-sm bg-gray-200 px-4 py-1 text-sm text-black whitespace-nowrap">UX Research</span>
-                  <span className="inline-block rounded-sm bg-gray-200 px-4 py-1 text-sm text-black whitespace-nowrap">B2B</span>
+                  <span className="inline-block rounded-sm bg-gray-200 px-4 py-1 text-sm text-black whitespace-nowrap">UX Design</span>
+                  <span className="inline-block rounded-sm bg-gray-200 px-4 py-1 text-sm text-black whitespace-nowrap">UI Design</span>
+                  <span className="inline-block rounded-sm bg-gray-200 px-4 py-1 text-sm text-black whitespace-nowrap">Prototyping</span>
+                  <span className="inline-block rounded-sm bg-gray-200 px-4 py-1 text-sm text-black whitespace-nowrap">User flow</span>
                 </div>
               </div>
             </div>
@@ -123,11 +125,11 @@ export default function Home() {
         {/* Thoughtful */}
 
         <div className="group relative overflow-hidden rounded-3xl bg-gray-100/80 backdrop-blur-sm p-8 h-[500px] transition-transform duration-300 hover:scale-[1.02] group">
-          <a href="/lilypad" className="w-full h-full">
+          <a href="/thoughtful" className="w-full h-full">
             <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between lg:gap-8 h-full">
               <div className="space-y-4 lg:w-1/3 lg:self-start">
                 <h3 className="text-2xl font-bold group-hover:text-secondary">Thoughtful</h3>
-                <p className="text-lg">Design and development mobile-first landing page for AI ed-tech startup</p>
+                <p className="text-lg">Coming Soon</p>
                 <div className="flex flex-wrap gap-2">
                   <span className="inline-block rounded-sm bg-gray-200 px-4 py-1 text-sm text-black whitespace-nowrap">UI Design</span>
                   <span className="inline-block rounded-sm bg-gray-200 px-4 py-1 text-sm text-black whitespace-nowrap">Landing Page</span>
@@ -138,7 +140,7 @@ export default function Home() {
                 <div className="relative w-full h-full">
                   <Image
                     src="/thoughtful-cover.png"
-                    alt="Lilypad Project"
+                    alt="Thoughtful Project"
                     className="rounded-lg object-contain lg:object-contain w-full h-full"
                     {...(typeof window !== 'undefined' && window.innerWidth >= 768 && window.innerWidth < 1024 ? { fill: true } : { width: 500, height: 300 })}
                   />
