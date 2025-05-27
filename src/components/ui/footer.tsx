a9194afec4548d446380d93ee73b108b79a9af75
Jessica Cheng <jessicacheng.code@gmail.com>
2025-05-27 15:34:47 -0400

    fix

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -6,17 +6,17 @@ export default function Footer() {
     <footer className="w-full bg-[#1C1C1C]">
       <div className="container max-w-7xl mx-auto px-8 py-4">
         <div className="flex flex-col md:flex-row justify-between items-center gap-4">
-          <div className="flex gap-4 flex-1 items-center">
+          <div className="flex gap-6 flex-1 items-center">
             <Link href="/#projects" className="text-white hover:text-secondary">
-              Projects
+              Works
             </Link>
             <Link href="/about" className="text-white hover:text-secondary">
               About
             </Link>
-            <Link href={`mailto:${process.env.NEXT_PUBLIC_EMAIL_URL}`} className="text-white hover:text-secondary">
+            <Link href="/contact" className="text-white hover:text-secondary">
               Contact
             </Link>
-            <Link href="https://www.linkedin.com/in/jessica-cheng-design-engineer/" className="text-white hover:text-secondary flex items-center gap-1" target="_blank" rel="noopener noreferrer">
+            <Link href="https://www.linkedin.com/in/design-jessicacheng" className="text-white hover:text-secondary flex items-center gap-1" target="_blank" rel="noopener noreferrer">
               LinkedIn
               <ArrowUpRight className="w-4 h-4 hover:text-secondary" />
             </Link>
