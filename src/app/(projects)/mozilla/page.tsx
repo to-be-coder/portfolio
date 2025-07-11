9cfdb98b5cc41e16278d9ffe9337018d13691e52
Jessica Cheng <jessicacheng.code@gmail.com>
2025-07-11 14:06:41 -0400

    wip

--- src/app/(projects)/mozilla/page.tsx
+++ src/app/(projects)/mozilla/page.tsx
@@ -14,7 +14,7 @@ export default function MozillaPage() {
         {/* Content */}
         <div className="relative z-10 flex-1 flex flex-col lg:flex-row w-full space-between justify-end lg:items-end my-10 border-b border-gray-200">
           <h1 className="flex-1 text-4xl md:text-5xl font-bold tracking-tight leading-tight text-black">Mozilla - Ghostcat</h1>
-          <p className="items-end text-lg md:text-2xl mb-1 text-black">On-device AI assistant</p>
+          <p className="items-end text-lg md:text-2xl mb-1 text-black">Privacy-first AI Assistant</p>
         </div>
       </div>
       <div className="w-full h-auto max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center rounded-lg overflow-hidden">
