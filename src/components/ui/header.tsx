50115b14256cd4c14077f1fd535bd27da2b3e736
Jessica Cheng <jessicacheng.code@gmail.com>
2025-07-11 14:05:35 -0400

    wip

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -88,8 +88,7 @@ export default function Header() {
         <div className="flex cursor-pointer">
           <Link href="/" legacyBehavior passHref>
             <div className="flex items-center gap-1">
-              <Image src="/icon.svg" alt="Jessica Cheng" width={40} height={40} />
-              <p className="text-md font-normal font-['Helvetica_Neue'] -m-1.5 p-1.5">Jessica Cheng</p>
+              <p className="text-lg font-semibold font-['Helvetica_Neue'] -m-1.5 p-1.5">Jessica Cheng</p>
             </div>
           </Link>
         </div>
