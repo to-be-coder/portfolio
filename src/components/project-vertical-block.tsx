85ccb330b6ead2267209444fc9fed2f50413a796
Jessica Cheng <jessicacheng.code@gmail.com>
2025-07-22 12:29:04 -0400

    wip

--- src/components/project-vertical-block.tsx
+++ src/components/project-vertical-block.tsx
@@ -9,11 +9,9 @@ interface SectionProps {
 
 export default function VerticalBlock({ title, children, titleColor = 'text-black', borderColor = 'border-[#000000]' }: SectionProps) {
   return (
-    <div className="flex flex-col">
-      <div className={`flex-1 border-t ${borderColor} pt-2`}>
-        <h2 className={`text-2xl font-regular mb-2 md:mb-4 ${titleColor}`}>{title}</h2>
-        {children}
-      </div>
+    <div className={`border-t ${borderColor} pt-2 flex flex-col mb-8 md:mb-12`}>
+      <h2 className={`text-2xl font-regular mb-2 md:mb-4 ${titleColor}`}>{title}</h2>
+      {children}
     </div>
   )
 }
