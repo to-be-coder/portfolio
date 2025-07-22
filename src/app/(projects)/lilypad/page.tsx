8c7cf034645652e0e24274e4f68631bcf6991892
Jessica Cheng <jessicacheng.code@gmail.com>
2025-07-22 10:55:28 -0400

    wip

--- src/app/(projects)/lilypad/page.tsx
+++ src/app/(projects)/lilypad/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import ProjectHeroSection from '@/components/project-hero-section'
 import CtaSection from '@/components/ui/cta'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { ArrowUpRight } from 'lucide-react'
@@ -10,16 +11,7 @@ export default function LilypadPage() {
   return (
     <main>
       {/* Hero Section */}
-      <div className="w-full h-auto flex flex-col relative max-w-7xl mx-auto px-8">
-        {/* Background Image with Overlay */}
-        <div className="relative z-10 flex-1 flex flex-col lg:flex-row w-full space-between justify-end lg:items-end my-10 border-b border-gray-200">
-          <h1 className="flex-1 text-4xl md:text-5xl font-bold tracking-tight leading-tight text-black">Lilypad</h1>
-          <p className="items-end text-lg md:text-2xl mb-1 text-black">EdTech Startup: Student Evaluation Service for School Districts</p>
-        </div>
-      </div>
-      <div className="w-full h-auto max-w-7xl mx-auto px-8 flex flex-col items-center rounded-lg overflow-hidden">
-        <Image src="/lilypad-hero.png" alt="Lilypad hero background" width={1000} height={100} className="w-full flex-1 object-cover max-h-[600px] object-center rounded-lg" />
-      </div>
+      <ProjectHeroSection title="Lilypad" subtitle="EdTech Startup: Student Evaluation Service for School Districts" imageSrc="/lilypad-hero.png" imageAlt="Lilypad hero background" />
 
       {/* Initial Overview and Challenges Section */}
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 my-16 lg:my-32 max-w-7xl">
