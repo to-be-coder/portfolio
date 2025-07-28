3ad496c2dfd00fe1894ac87df9080747f7379924
Jessica Cheng <jessicacheng.code@gmail.com>
2025-07-28 09:32:05 -0400

    wip

--- src/components/home-hero-section.tsx
+++ src/components/home-hero-section.tsx
@@ -2,7 +2,7 @@ import Chat from './chat'
 
 export default function HomeHeroSection() {
   return (
-    <div className="relative w-full h-full max-w-7xl mx-auto pt-8 px-4 sm:px-6 lg:px-8">
+    <div data-home-hero-section className="relative w-full h-full max-w-7xl mx-auto pt-8 px-4 sm:px-6 lg:px-8">
       <Chat />
     </div>
   )
