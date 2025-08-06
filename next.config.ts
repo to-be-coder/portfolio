d693cc293633abeb63cfca591e63a105e77ada55
Jessica Cheng <jessicacheng.code@gmail.com>
2025-08-06 16:52:09 -0400

    hot fix

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: (process.env.NEXT_PUBLIC_IMAGE_PROTOCOL as 'https' | 'http') || 'https',
-        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME || 'prod-files-secure.s3.us-west-2.amazonaws.com',
-        port: process.env.NEXT_PUBLIC_IMAGE_PORT || '',
-        pathname: process.env.NEXT_PUBLIC_IMAGE_PATHNAME || '/**',
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
