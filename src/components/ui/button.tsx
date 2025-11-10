92988cb3de321ebe0a44fe62f6b366a04c7b5a76
Jessica Cheng <jessicacheng.code@gmail.com>
2025-11-10 15:39:00 -0500

    wip

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -50,7 +50,7 @@ const CallToActionButton = React.forwardRef<HTMLButtonElement, CallToActionButto
   const [isHovered, setIsHovered] = React.useState(false)
 
   const iconElement = (
-    <span className={cn('flex items-center justify-center rounded-full transition-all duration-300 shrink-0 overflow-visible', isHovered ? 'w-6 h-6 bg-white' : 'w-2 h-2 bg-black')}>
+    <span className={cn('flex items-center justify-center rounded-full transition-all duration-300 shrink-0 overflow-visible', isHovered ? 'w-6 h-6 bg-white' : 'w-2 h-2 bg-white')}>
       <span
         className={cn('text-black [&_svg]:w-3 [&_svg]:h-3 ease-out', isHovered ? 'translate-x-0 opacity-100 transition-all duration-500' : '-translate-x-[200%] opacity-0 transition-all duration-200')}
       >
@@ -65,7 +65,7 @@ const CallToActionButton = React.forwardRef<HTMLButtonElement, CallToActionButto
       <Button
         ref={ref}
         size={size}
-        className={cn('hover:pl-2 group relative rounded-full bg-gray-200 text-black transition-all duration-300 hover:bg-black hover:text-white', className)}
+        className={cn('hover:pl-2 group relative rounded-full bg-black text-white transition-all duration-300 hover:bg-black hover:text-white', className)}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
         asChild={asChild}
