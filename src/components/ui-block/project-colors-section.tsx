cfa7cf4f314e96bedd387c802e6777d999998c27
Jessica Cheng <jessicacheng.code@gmail.com>
2025-11-12 13:52:55 -0500

    wip

--- src/components/ui-block/project-colors-section.tsx
+++ src/components/ui-block/project-colors-section.tsx
@@ -1,4 +1,5 @@
 import ProjectSectionTitle from './project-section-title'
+import { HorizontalStack } from './project-stack'
 
 interface Color {
   hex: string
@@ -15,21 +16,19 @@ interface ProjectColorsSectionProps {
 
 export default function ProjectColorsSection({ title, titleColor, dotColor, colors }: ProjectColorsSectionProps) {
   return (
-    <div className="container mx-auto py-16 lg:py-32 max-w-6xl">
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        <div className="mb-8">
-          <ProjectSectionTitle color={titleColor} dotColor={dotColor}>
-            {title}
-          </ProjectSectionTitle>
-        </div>
-        {colors.map((color, i) => (
-          <div className="flex flex-col" key={color.hex + i}>
-            <div className={` rounded-xl flex items-end p-4 aspect-square ${color.border ? 'border border-gray-200' : ''}`} style={{ backgroundColor: color.hex }}>
-              <span className={color.textClass || ''}>{color.hex}</span>
-            </div>
-          </div>
-        ))}
+    <HorizontalStack mobileCols={2} desktopCols={4}>
+      <div className="mb-8">
+        <ProjectSectionTitle color={titleColor} dotColor={dotColor}>
+          {title}
+        </ProjectSectionTitle>
       </div>
-    </div>
+      {colors.map((color, i) => (
+        <div className="flex flex-col" key={color.hex + i}>
+          <div className={` rounded-xl flex items-end p-4 aspect-square ${color.border ? 'border border-gray-200' : ''}`} style={{ backgroundColor: color.hex }}>
+            <span className={color.textClass || ''}>{color.hex}</span>
+          </div>
+        </div>
+      ))}
+    </HorizontalStack>
   )
 }
--- src/components/ui-block/project-stack.tsx
+++ src/components/ui-block/project-stack.tsx
@@ -11,8 +11,8 @@ interface VerticalStackProps {
 export function VerticalStack({ title, children, titleColor = 'text-black', borderColor = 'border-[#000000]', className = '' }: VerticalStackProps) {
   return (
     <div className={`${borderColor} flex flex-col ${className}`}>
-      <h2 className={`text-2xl mb-2 md:mb-4 ${titleColor}`}>{title}</h2>
-      <div className="flex flex-col gap-y-2">{children}</div>
+      {title && <h2 className={`text-2xl mb-2 md:mb-4 ${titleColor}`}>{title}</h2>}
+      <div className="flex flex-col gap-y-4">{children}</div>
     </div>
   )
 }
