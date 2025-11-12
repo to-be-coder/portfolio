eedbd8d0ef3f193065d942fc93562ec52a493cc0
Jessica Cheng <jessicacheng.code@gmail.com>
2025-11-12 18:00:47 -0500

    wip

--- src/app/(projects)/hobby/page.tsx
+++ src/app/(projects)/hobby/page.tsx
@@ -21,7 +21,7 @@ export default function Hobby() {
           {/* Images */}
           {[...Array(40)].map((_, i) => (
             <div key={i} className="break-inside-avoid">
-              <Image src={`/hobby-${i + 1}.jpeg`} alt={`Hobby Image ${i + 1}`} className="rounded-md w-full h-auto object-cover" width={600} height={600} />
+              <Image src={`/hobby-${i + 1}.jpeg`} alt={`Hobby Image ${i + 1}`} className="rounded-xl w-full h-auto object-cover" width={600} height={600} />
             </div>
           ))}
         </div>
